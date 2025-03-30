b5c1dc6c51a6b059d2bb991a50c68745af9bdd00
Rowney Ouma <oumarowney@gmail.com>
2025-03-30 16:22:02 +0300

    all tasks complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+//Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+
+let num3 =25;
+let num4 = 7;
+let mod = num3%num4
+
+let max = Math.max(11,14,9,20, 7)
+console.log(max)
